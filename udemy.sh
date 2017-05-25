a4678e50cd3b6c8463a7b8a17b317796e610e35f
Michael Chon <mchon89@gmail.com>
2017-05-24 20:01:27 -0400

    Update udemy.sh

--- udemy.sh
+++ udemy.sh
@@ -5,7 +5,7 @@ do
 url="https://www.udemy.com/api-2.0/courses/?page_size=100&page="
 url=$url$i  
 echo $url
-curl --user SrBh8o75qcbH6mpOiMiWj4Mno1bGnkP4cqq4kKO9:SSqv8QBG2FcLAn34X1eMZH7xT5tdLS8gan7HmQ1Y1brHkegAJrzz9r9d5CrYLE2mB077H111HbHLVamyCCjXNxDXM8gW23Wbm4xfcvsceaOo5D34x6hQdvR2bZ35XWQl $url > 1234.json | python udemy_fixed.py
+curl --user key:private key $url > 1234.json | python udemy_fixed.py
 done
 
 exit 0
